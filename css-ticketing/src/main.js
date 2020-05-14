6d544c593644f282f7eccd5db77942aecb5148d2
YoussefMIbrahim <kn1010az@go.minnstate.edu>
2020-05-13 19:18:05 -0500

    stuff

--- css-ticketing/src/main.js
+++ css-ticketing/src/main.js
@@ -1,5 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+
+import TicketAPIServies from '@/services/TicketServices'
+
+
+Vue.prototype.$ticket_api = TicketAPIServies
 
 Vue.config.productionTip = false
 
